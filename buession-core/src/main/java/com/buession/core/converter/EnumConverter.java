1d54b927230ab04af9a4ff2a245f2b018b6a274f
Yong.Teng <webmaster@buession.com>
2022-03-14 13:20:12 +0800

    修复 EnumConverter 转换bug

--- buession-core/src/main/java/com/buession/core/converter/EnumConverter.java
+++ buession-core/src/main/java/com/buession/core/converter/EnumConverter.java
@@ -19,7 +19,7 @@
  * +-------------------------------------------------------------------------------------------------------+
  * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										       |
  * | Author: Yong.Teng <webmaster@buession.com> 													       |
- * | Copyright @ 2013-2021 Buession.com Inc.														       |
+ * | Copyright @ 2013-2022 Buession.com Inc.														       |
  * +-------------------------------------------------------------------------------------------------------+
  */
 package com.buession.core.converter;
@@ -58,7 +58,7 @@ public class EnumConverter<E extends Enum<E>> implements Converter<String, E> {
 		}
 
 		if(result == null){
-			result = EnumUtils.valueOf(enumType, source.toUpperCase());
+			result = EnumUtils.valueOf(enumType, source.toLowerCase());
 		}
 
 		return result;
