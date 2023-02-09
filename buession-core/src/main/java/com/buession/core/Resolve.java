2c3372c9f60bd2fbd931d4ad5ef8e7a3b9948591
Yong.Teng <webmaster@buession.com>
2023-02-09 12:40:46 +0800

    Executor、Resolve 增加异常支持

--- buession-core/src/main/java/com/buession/core/Resolve.java
+++ buession-core/src/main/java/com/buession/core/Resolve.java
@@ -19,7 +19,7 @@
  * +-------------------------------------------------------------------------------------------------------+
  * | License: http://www.apache.org/licenses/LICENSE-2.0.txt 										       |
  * | Author: Yong.Teng <webmaster@buession.com> 													       |
- * | Copyright @ 2013-2022 Buession.com Inc.														       |
+ * | Copyright @ 2013-2023 Buession.com Inc.														       |
  * +-------------------------------------------------------------------------------------------------------+
  */
 package com.buession.core;
@@ -45,7 +45,10 @@ public interface Resolve<S, T> {
 	 * 		待解析对象
 	 *
 	 * @return 解析结果
+	 *
+	 * @throws Exception
+	 * 		异常
 	 */
-	T resolve(final S source);
+	T resolve(final S source) throws Exception;
 
 }
