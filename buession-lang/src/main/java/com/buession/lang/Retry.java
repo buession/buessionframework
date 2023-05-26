fba4d4a4396550be210d89b56e70c9326658b7ca
Yong.Teng <webmaster@buession.com>
2023-05-27 00:33:51 +0800

    【lang】新增重试配置 Retry

--- buession-lang/src/main/java/com/buession/lang/Retry.java
+++ buession-lang/src/main/java/com/buession/lang/Retry.java
@@ -21,10 +21,116 @@
  * | Author: Yong.Teng <webmaster@buession.com> 													       |
  * | Copyright @ 2013-2023 Buession.com Inc.														       |
  * +-------------------------------------------------------------------------------------------------------+
- */package com.buession.lang;/**
- * 
+ */
+package com.buession.lang;
+
+import java.io.Serializable;
+import java.time.Duration;
+
+/**
+ * 重试配置
  *
  * @author Yong.Teng
  * @since 2.3.0
- */public class Retry {
+ */
+public class Retry implements Serializable {
+
+	private final static long serialVersionUID = -4074820402227945905L;
+
+	/**
+	 * 最大重数次数
+	 */
+	private int maxAttempts = 3;
+
+	/**
+	 * 第一次和第二次尝试重试时间间隔，默认：1 秒
+	 */
+	private Duration initialInterval = Duration.ofMillis(1000);
+
+	/**
+	 * 应用于上一重试间隔的乘数
+	 */
+	private double multiplier = 1.0;
+
+	/**
+	 * 最大重试时间间隔，默认：10 秒
+	 */
+	private Duration maxInterval = Duration.ofMillis(10000);
+
+	/**
+	 * 返回最大重数次数
+	 *
+	 * @return 最大重数次数
+	 */
+	public int getMaxAttempts(){
+		return this.maxAttempts;
+	}
+
+	/**
+	 * 设置最大重数次数
+	 *
+	 * @param maxAttempts
+	 * 		最大重数次数
+	 */
+	public void setMaxAttempts(int maxAttempts){
+		this.maxAttempts = maxAttempts;
+	}
+
+	/**
+	 * 返回第一次和第二次尝试重试时间间隔
+	 *
+	 * @return 第一次和第二次尝试重试时间间隔
+	 */
+	public Duration getInitialInterval(){
+		return this.initialInterval;
+	}
+
+	/**
+	 * 设置第一次和第二次尝试重试时间间隔
+	 *
+	 * @param initialInterval
+	 * 		第一次和第二次尝试重试时间间隔
+	 */
+	public void setInitialInterval(Duration initialInterval){
+		this.initialInterval = initialInterval;
+	}
+
+	/**
+	 * 返回应用于上一重试间隔的乘数
+	 *
+	 * @return 应用于上一重试间隔的乘数
+	 */
+	public double getMultiplier(){
+		return this.multiplier;
+	}
+
+	/**
+	 * 设置应用于上一重试间隔的乘数
+	 *
+	 * @param multiplier
+	 * 		应用于上一重试间隔的乘数
+	 */
+	public void setMultiplier(double multiplier){
+		this.multiplier = multiplier;
+	}
+
+	/**
+	 * 返回最大重试时间间隔
+	 *
+	 * @return 最大重试时间间隔
+	 */
+	public Duration getMaxInterval(){
+		return this.maxInterval;
+	}
+
+	/**
+	 * 设置最大重试时间间隔
+	 *
+	 * @param maxInterval
+	 * 		最大重试时间间隔
+	 */
+	public void setMaxInterval(Duration maxInterval){
+		this.maxInterval = maxInterval;
+	}
+
 }
