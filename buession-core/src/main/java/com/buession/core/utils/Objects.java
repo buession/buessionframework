ed7a6c512dc979ab96d906c17c882999a7459ac0
Yong.Teng <webmaster@buession.com>
2024-11-13 16:10:22 +0800

    清理 redis 文件

--- buession-core/src/main/java/com/buession/core/utils/Objects.java
+++ buession-core/src/main/java/com/buession/core/utils/Objects.java
@@ -21,77 +21,10 @@
  * | Author: Yong.Teng <webmaster@buession.com> 													       |
  * | Copyright @ 2013-2024 Buession.com Inc.														       |
  * +-------------------------------------------------------------------------------------------------------+
- */
-package com.buession.redis.transaction;
-
-import com.buession.redis.core.FutureResult;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
-import java.util.stream.Collectors;
-
-/**
- * 事务代理抽象类
- *
- * @param <T>
- * 		原生事务类型
- * @param <FR>
- * 		事务异步结果
+ */package com.buession.core.utils;/**
+ * 
  *
  * @author Yong.Teng
- * @since 3.0.0
- */
-public abstract class AbstractTransactionProxy<T, FR extends FutureResult<?>> implements TransactionProxy<T, FR> {
-
-	private Transaction target;
-
-	private final T object;
-
-	private final Queue<FR> txResults = new LinkedList<>();
-
-	private final Logger logger = LoggerFactory.getLogger(getClass());
-
-	public AbstractTransactionProxy(final Transaction target, final T object) {
-		//Assert.isNull(target, "Redis Transaction cloud not be null.");
-		this.target = target;
-		this.object = object;
-	}
-
-	@Override
-	public T getObject() {
-		return object;
-	}
-
-	@Override
-	public Queue<FR> getTxResults() {
-		return txResults;
-	}
-
-	@Override
-	public List<Object> exec() {
-		logger.info("Redis transaction exec.");
-		target.exec();
-		return txResults.stream().map((r)->r.convert(r.get())).collect(Collectors.toList());
-	}
-
-	@Override
-	public String discard() {
-		logger.info("Redis transaction discard.");
-		return target.discard();
-	}
-
-	@Override
-	public void close() {
-		logger.info("Redis pipeline close.");
-		txResults.clear();
-		target.close();
-	}
-
-	protected void setTarget(Transaction target) {
-		this.target = target;
-	}
-
+ * @since 3.0.1
+ */public class Objects {
 }
