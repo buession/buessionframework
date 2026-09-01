7d5bd74ad5471bed8e2dc5e9cd57903e3f40ea2e
eduosi <eduosi@users.noreply.github.com>
2026-09-01 10:19:22 +0800

    【部署】：2026-09-01 10:19:21

---
+++ js/stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?f04c577c4cd5d961fad5cf491559e973";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
